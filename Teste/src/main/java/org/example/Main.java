8138d8d7d123f570004cc1da708d954d52f216f4
Daniel Nardi <145508288+DanielNardi@users.noreply.github.com>
2024-09-19 01:06:29 -0300

    Main.java
    sim

--- Teste/src/main/java/org/example/Main.java
+++ Teste/src/main/java/org/example/Main.java
@@ -1,26 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static org.example.primo.numeroPrimo;
+
+
 public class Main {
     public static void main(String[] args) {
+
+
+
 
         //clique inicia em 1 cont 0
         int clique = 1;
         int acumulado =0;
 
         for (int i = 0; i < 10; i++) {
-
+            //se for maior que 21 soma +1
             if (acumulado < 21) {
                 acumulado += clique;
             } else {
                 acumulado += 1;
             }
-
-            if (acumulado/2 == 0) {
-                System.out.println(acumulado + " é um número primo");
+            //função que verifica numero primo
+            if (numeroPrimo(acumulado)) {
+                System.out.printf("%d é um número primo%n", acumulado);
             }
-
 
             System.out.println(clique +"º" + " Acumulado: " + acumulado);
             clique++;
